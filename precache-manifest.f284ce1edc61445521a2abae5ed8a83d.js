180d3f90704fb16755e9ca3a86bfe54dda49146b
pabloMarinoArg <31575175+pabloMarinoArg@users.noreply.github.com>
2020-09-27 19:03:30 -0300

    Updates

---
+++ precache-manifest.f284ce1edc61445521a2abae5ed8a83d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f0dd40725bccb64f4a12ec58fdbbef",
+    "url": "/blogre/index.html"
+  },
+  {
+    "revision": "d8d4a89db106a6460653",
+    "url": "/blogre/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "7eeacf68d3aac0144119",
+    "url": "/blogre/static/js/2.6a27e046.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blogre/static/js/2.6a27e046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d4a89db106a6460653",
+    "url": "/blogre/static/js/main.42cdbd7b.chunk.js"
+  },
+  {
+    "revision": "8f096f4268e00d9bc63f",
+    "url": "/blogre/static/js/runtime-main.6304fab1.js"
+  }
+]);
